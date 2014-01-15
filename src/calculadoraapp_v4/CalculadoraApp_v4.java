b48ff1dd35eaf9676cbd60b1295d2e149500c16a
becari <becari@Likekit>
2014-01-15 16:59:53 +0100

    Algunos métodos agregados.

--- src/calculadoraapp_v4/CalculadoraApp_v4.java
+++ src/calculadoraapp_v4/CalculadoraApp_v4.java
@@ -6,17 +6,38 @@
 
 package calculadoraapp_v4;
 
-/**
- *
+/*
  * @author Marc
  */
 public class CalculadoraApp_v4 {
 
+    public static String num1;
+    public static String num2;
+    public static String operador;
+    public static int resultado;
+    public static boolean calOn;
+    
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) { 
+        calOn = false;
+        num1 = null;
+        num2 = null;
+        operador = null;
+        resultado = 0;
+         
+        encenderCalculadora();
+        capturarTeclas();
+    }
+    public static void encenderCalculadora(){
+        calOn = true;
+        System.out.println("Calculadora encendida");
+    }
+    public static void capturarTeclas(){
+        while(calOn == true){
+            
+        }
     }
     
 }
